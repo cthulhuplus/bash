957823740c67ff184018f668195bf4694ea34e21
root <root@localhost.localdomain>
2017-01-09 14:59:46 -0500

    Lamp provision script, still wonky

---
+++ lamp.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+### Script is based off of:
+### https://www.howtoforge.com/apache_php_mysql_on_centos_7_lamp
+
+# Set up EPEL Repo for phpMyAdmin
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY*
+yum -y install epel-release
+
+# Install MariaDB
+yum -y install mariadb-server mariadb
+systemctl start mariadb.service
+systemctl enable mariadb.service
+
+#Set Mysql root password
+mysqladmin -u root password NewPassword123
+#Delete anonymous MySQL users
+mysql -u root -p 'NewPassword123' -e "delete from mysql.user where User='';"
+#Disable remote MySQL access for root MySQL user
+mysql -u root -p 'NewPassword123' -e "delete from mysql.user where User='root' and host not in ('localhost', '127.0.0.1', '::1');"
+#Remove the test database
+mysql -u root -p 'NewPassword123' -e "drop database test;"
+mysql -u root -p 'NewPassword123' -e "delete from mysql.db where Db='test' or Db='test\_%';"
+mysql -u root -p 'NewPassword' -e "flush privileges;"
+
+#Install Apache
+yum -y install httpd
+systemctl start httpd.service
+systemctl enable httpd.service
+
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=https
+firewall-cmd --reload
+
+#Install PHP
+yum -y install php
+
+systemctl restart httpd.service
+
+touch /var/www/html/info.php
+echo "<?php phpinfo(); ?>" > /var/www/html/info.php
+chown httpd:httpdp /var/www/html/info.php
+
+#Install phpMyAdmin
+yum -y install phpmyadmin
+
+### phpMyAdmin Configuration needs to be added
